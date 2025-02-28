678eeae6ac07f0957412409eab51c1fbe3fc69b9
Siret Eller <sireteller@gmail.com>
2025-02-28 22:46:06 +0200

    feat: add basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './views/home/home.component';
+import { CategoryComponent } from './views/category/category.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Lavendlik',
+    loadComponent: () =>
+      import('./views/home/home.component').then((c) => c.HomeComponent),
+  },
+  {
+    path: 'category',
+    title: 'Category | Lavendlik',
+    loadComponent: () =>
+      import('./views/category/category.component').then(
+        (c) => c.CategoryComponent,
+      ),
+  },
+];
