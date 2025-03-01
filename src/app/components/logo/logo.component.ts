5d2ec60e25e236e02e6cc1c6a1226838144772af
Siret Eller <sireteller@gmail.com>
2025-03-01 19:18:17 +0200

    feat(logo): new component 'logo'

---
+++ src/app/components/logo/logo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  imports: [],
+  templateUrl: './logo.component.html',
+  standalone: true,
+  styleUrl: './logo.component.css',
+})
+export class LogoComponent {}
